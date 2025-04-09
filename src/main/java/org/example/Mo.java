7e005c608708db3d5d7ca49d03f6b1345be3bf0a
unknown <skooy90@gmail.com>
2025-04-09 21:24:19 +0900

    list 추가 설정과 클래스 분리

---
+++ src/main/java/org/example/Mo.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Mo {
+  int id;
+  String content;
+  String author;
+
+  Mo(int id, String content, String author) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+
+}
--- src/main/java/org/example/app.java
+++ src/main/java/org/example/app.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class App {
   private Scanner sc;
   private int quoteId = 1; // 명언 번호 (자동 증가)
-  private ArrayList<mo> quoteList = new ArrayList<>(); // 명언 저장소
+  private ArrayList<Mo> quoteList = new ArrayList<>(); // 명언 저장소
+  private int num = 0;
 
   App(Scanner sc){
     this.sc = sc;
@@ -26,21 +27,28 @@ public class App {
         System.out.print("작가: ");
         String author = sc.nextLine().trim();
 
-        mo quote = new mo(quoteId, content, author);
+        Mo quote = new Mo(quoteId, content, author);
         quoteList.add(quote);
 
         System.out.printf("%d번 명언이 등록되었습니다.\n", quoteId);
         quoteId++;
+        num++;
       }
       else if (cmd.equals("list")) {
-        System.out.println("번호 / 작가 / 명언");
+        System.out.println("번호 / 인물 / 명언");
         System.out.println("----------------------");
+        
 
+        // quoteList.isEmpty()은 배열에서 값이 비어있는지 확인하는 코드
+        if (quoteList.isEmpty()) {
+          System.out.println("등록된 명언이 없습니다.");
+        }
         // 최신 등록이 위로 올라오게 역순 출력
         for (int i = quoteList.size() - 1; i >= 0; i--) {
-          mo q = quoteList.get(i);
+          Mo q = quoteList.get(i);
           System.out.printf("%d / %s / %s\n", q.id, q.author, q.content);
         }
+          System.out.println(num+"개의 명언이 등록되었습니다.");
       }
       else if (cmd.equals("exit")) {
         System.out.println("== motivation 앱 종료 ==");
@@ -50,17 +58,5 @@ public class App {
         System.out.println("알 수 없는 명령어입니다.");
       }
     }
-  }
-}
-
-class mo {
-  int id;
-  String content;
-  String author;
-
-  mo(int id, String content, String author) {
-    this.id = id;
-    this.content = content;
-    this.author = author;
   }
 }
