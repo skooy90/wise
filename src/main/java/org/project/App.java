df90c27ff161c3f599429fc8006261da3c3d52f2
unknown <skooy90@gmail.com>
2025-04-10 21:26:03 +0900

    parsing 적용

--- src/main/java/org/project/App.java
+++ src/main/java/org/project/App.java
@@ -3,8 +3,7 @@ package org.project;
 import org.system.controller.SystemController;
 import org.wiseSaying.controller.WiseSayingController;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
   App() {
@@ -22,6 +21,30 @@ public class App {
         wiseSayingController.add();
       } else if (cmd.equals("list")) {
         wiseSayingController.list();
+      } else if (cmd.startsWith("delete")){
+            //parsing 기존에 들어온 개념을 쪼개서 나눈다.
+          String[] cmdBits = cmd.split("\\?");
+//          for(String i : cmdBits){
+//        System.out.println(i);
+        String actionCode = cmdBits[0];
+          Map<String, String> params = new HashMap<>();
+
+        String[] paramBits = cmdBits[1].split("=", 2);
+
+        String key = paramBits[0];
+        String value =paramBits[1];
+        params.put(key, value);
+        System.out.println(Arrays.toString(cmdBits));
+        System.out.println("actioncode : " + actionCode);
+        System.out.println("key : " + key);
+        System.out.println("value : " + value);
+
+        // parsing end
+
+        wiseSayingController.delete();
+
+
+
       } else if (cmd.equals("exit")) {
         systemController.exit();
         break;
--- src/main/java/org/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/org/wiseSaying/controller/WiseSayingController.java
@@ -40,4 +40,7 @@ public class WiseSayingController {
 
     }
   }
+  public void delete(){
+
+  }
 }
